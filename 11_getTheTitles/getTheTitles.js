a30ae0624205efddbc4c1a5d77f4b8b8e8ab0b4e
gorilla-san <shonemoc@gmail.com>
2022-07-21 12:07:29 +0200

    findTheOldest cheatCodesOn

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
-const getTheTitles = function() {
+const getTheTitles = function(myArray = []) {
+        let i 
+        let len = myArray.length
+        const result = []
+        for (i=0; i<len; i++) {
+            result[i] = myArray[i].title 
+        }
+        console.log(result)
+        return result
 
 };
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+getTheTitles(books)
 
 // Do not edit below this line
 module.exports = getTheTitles;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,49 @@
-const findTheOldest = function() {
+const findTheOldest = function(myArray) {
+    console.log(myArray)
+    len = myArray.length 
+    let i 
+    for (i=0; i<len; i++) {
+        if (myArray[i].yearOfDeath === undefined) {
+            myArray[i].yearOfDeath = new Date().getFullYear();      
+        }
+    }
+    function compare( a, b ) {
+        if ( (a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth) ){
+          return -1;
+        }
+        if ( (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ){
+          return 1;
+        }
+        return 0;
+      }
+      
+      sortedArray = myArray.sort( compare );
+      console.log(sortedArray.at(-1))
+      console.log(sortedArray[0].yearOfDeath, sortedArray[0].name)
 
-};
+      return sortedArray.at(-1)
+
+}
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1066,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+]
+
+findTheOldest(people)
+    
 
 // Do not edit below this line
 module.exports = findTheOldest;
