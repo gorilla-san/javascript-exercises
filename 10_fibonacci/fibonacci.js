33fc25f13e629d9055c485f31ce8020bfc8c6ca1
gorilla-san <shonemoc@gmail.com>
2022-07-21 10:26:18 +0200

    fibBro

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let fibN = Math.floor((Math.pow((Math.sqrt(5)+1)/2, n) - (Math.pow(-1, n))/(Math.pow((Math.sqrt(5)+1)/2, n)))/(Math.sqrt(5)))
+    if (n>0) {
+    console.log(+fibN)
+    return +fibN
+    }
+    else console.log("OOPS"); return "OOPS"
+};  
 
-};
+fibonacci(50)
 
 // Do not edit below this line
 module.exports = fibonacci;
